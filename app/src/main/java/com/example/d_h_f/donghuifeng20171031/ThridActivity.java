7bc01c2be9994de5fb8e1f4ba330a7c1875dce3c
d-h-f <33215710+d-h-f@users.noreply.github.com>
2017-10-31 13:13:59 +0800

    Update ThridActivity.java

--- app/src/main/java/com/example/d_h_f/donghuifeng20171031/ThridActivity.java
+++ app/src/main/java/com/example/d_h_f/donghuifeng20171031/ThridActivity.java
@@ -9,5 +9,7 @@ public class ThridActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thrid);
+        
+        //33333333333333333
     }
 }
